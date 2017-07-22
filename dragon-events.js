cccb89a94acf31b619ee092a7b98e97dcd12754c
David Poindexter <david.poindexter@blackboard.com>
2017-07-22 11:08:08 -0400

    Add examples of regualar ES5 functions

--- dragon-events.js
+++ dragon-events.js
@@ -1,1 +1,23 @@
-console.log('It works!');
+const dragonEvents = [
+  { type: 'attack', value: 12, target: 'player-1' },
+  { type: 'yawn', value: 40},
+  { type: 'eat', target: 'horse' },
+  { type: 'attack', value: 23, target: 'player-2' },
+  { type: 'attack', value: 12, target: 'player-1' },
+];
+
+const totalDamageOnPlayer1 = dragonEvents
+  .filter(function(event) {
+    return event.type === 'attack';
+  })
+  .filter(function(event) {
+    return event.target === 'player-1';
+  })
+  .map(function(event) {
+    return event.value;
+  })
+  .reduce(function(prev, value) {
+    return prev + value;
+  }, 0);
+
+console.log('totalDamageOnPlayer1\n', totalDamageOnPlayer1);
